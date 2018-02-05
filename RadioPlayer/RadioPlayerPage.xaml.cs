a3860114da237ed6c032359dcde8c64b4b429b14
Karol Pliszka <karpl10@hotmail.com>
2018-02-05 23:56:21 +0100

    początek

---
+++ RadioPlayer/RadioPlayerPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace RadioPlayer
+{
+    public partial class RadioPlayerPage : ContentPage
+    {
+        public RadioPlayerPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
